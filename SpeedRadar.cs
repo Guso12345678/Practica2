56c59819aee48b7e7a029d3706e64981ae95b93a
Guzman <202206388@alu.comillas.edu>
2024-10-01 09:28:44 +0200

    Adaptacion del metodo mediante el añadido de una nueva funcion

--- SpeedRadar.cs
+++ SpeedRadar.cs
@@ -1,18 +1,20 @@
 ﻿namespace Practice1
 {
-    class SpeedRadar : IMessageWritter
+    public class SpeedRadar : IMessageWritter
     {
         //Radar doesn't know about Vechicles, just speed and plates
         private string plate;
         private float speed;
         private float legalSpeed = 50.0f;
         public List<float> SpeedHistory { get; private set; }
+        public List<string> SpeedingVehicles { get; private set; }
 
         public SpeedRadar()
         {
             plate = "";
             speed = 0f;
             SpeedHistory = new List<float>();
+            SpeedingVehicles = new List<string>();
         }
 
         public void TriggerRadar(Vehicle vehicle)
@@ -20,12 +22,29 @@
             plate = vehicle.GetPlate();
             speed = vehicle.GetSpeed();
             SpeedHistory.Add(speed);
+            if (speed > legalSpeed)
+            {
+                SpeedingVehicles.Add(plate);  // Guardamos la matrícula si excede la velocidad
+            }
         }
-        
+
+        public bool GetLastReading2()
+        {
+            if (speed > legalSpeed)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         public string GetLastReading()
         {
             if (speed > legalSpeed)
             {
+
                 return WriteMessage("Catched above legal speed.");
             }
             else
