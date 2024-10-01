7115e4ca79f04127aafa49ea000170475dbdb951
Guzman <202206388@alu.comillas.edu>
2024-10-01 09:13:57 +0200

    Añadido de la nueva clase de City

---
+++ City.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practice1;
+
+namespace Practica_2
+{
+    public class City
+    {
+        private PolicecarStation policecarstation;
+        public List<string> taxisList = new List<string>();
+        public City()
+        {
+            policecarstation = new PolicecarStation();
+        }
+
+        public void AddTaxis(Vehicle vehicle)
+        {
+            if (!taxisList.Contains(vehicle.GetPlate()))
+            {
+                taxisList.Add(vehicle.GetPlate());
+
+                foreach (string taxis in taxisList)
+                {
+                    Console.WriteLine(taxis);
+                }
+            }
+        }
+    }
+}
+
