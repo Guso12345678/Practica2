63ddd9b2983d314dca9ebc208c224e846dbfb454
Guzman <202206388@alu.comillas.edu>
2024-10-01 09:26:36 +0200

    Adaptacion del metodo para las nuevas funcionalidades

--- PoliceCar.cs
+++ PoliceCar.cs
@@ -1,16 +1,20 @@
 ﻿namespace Practice1
 {
-    class PoliceCar : Vehicle
+    public class PoliceCar : Vehicle
     {
         //constant string as TypeOfVehicle wont change allong PoliceCar instances
-        private const string typeOfVehicle = "Police Car"; 
+        private const string typeOfVehicle = "Police Car";
         private bool isPatrolling;
+        private bool isPersecution;
         private SpeedRadar speedRadar;
+        private PolicecarStation policecarStation;
 
         public PoliceCar(string plate) : base(typeOfVehicle, plate)
         {
             isPatrolling = false;
+            isPersecution = false;
             speedRadar = new SpeedRadar();
+            policecarStation = new PolicecarStation();
         }
 
         public void UseRadar(Vehicle vehicle)
@@ -57,13 +61,37 @@
                 Console.WriteLine(WriteMessage("was not patrolling."));
             }
         }
-
         public void PrintRadarHistory()
         {
             Console.WriteLine(WriteMessage("Report radar speed history:"));
             foreach (float speed in speedRadar.SpeedHistory)
             {
                 Console.WriteLine(speed);
+            }
+        }
+        public void ActivateAlarm()
+        {
+            if (speedRadar.GetLastReading2() == true)
+            {
+                policecarStation.Alert = true;
+
+                foreach (string plate in speedRadar.SpeedingVehicles)
+                {
+                    Console.WriteLine($"{plate} are fugitives");
+                }
+            }
+        }
+        public void InitiatePersecution(Vehicle vehicle)
+        {
+            if (policecarStation.Alert == true & isPatrolling == true & speedRadar.SpeedingVehicles.Contains(vehicle.GetPlate()) & isPersecution == false)
+            {
+                isPersecution = true;
+                Console.WriteLine($"Se inicia una persecucion a : {vehicle.GetPlate()}");
+            }
+            else
+            {
+                isPersecution = false;
+                Console.WriteLine($"No se puede iniciar una persecucion");
             }
         }
     }
