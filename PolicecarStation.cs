29bcd487386521f38315a63915b1862b9569cfb2
Guzman <202206388@alu.comillas.edu>
2024-10-01 09:18:36 +0200

    Creacion de la clase PolicecarStation

---
+++ PolicecarStation.cs
@@ -0,0 +1,29 @@
+﻿namespace Practice1
+{
+    public class PolicecarStation
+    {
+        private bool alert = false;
+        public List<string> polices_cars = new List<string>();
+        private double legalSpeed;
+        private SpeedRadar speedRadar;
+        public PolicecarStation()
+        {
+            speedRadar = new SpeedRadar();
+        }
+        public bool Alert
+        {
+            get { return alert; }
+            set { alert = value; }
+        }
+        public double LegalSpeed
+        {
+            get { return legalSpeed; }
+            set { legalSpeed = value; }
+        }
+        public void AddpolicesCars(Vehicle vehicle)
+        {
+            polices_cars.Add(vehicle.GetPlate());
+        }
+    }
+}
+
